636edabc7f6d602fd988ccc6f10bb7ed38a3f8d8
Shangway Hsu <shangway.hsu@gmail.com>
2017-02-08 22:11:33 -0800

    Fix typo in home page column

--- routes/viewChores.js
+++ routes/viewChores.js
@@ -1,3 +1,20 @@
 exports.view = function(req, res){
-   res.render('viewChores');
+   res.render('viewChores', {
+     'mychores' : [
+       {
+         name: 'Clean Dishes',
+         assignedTo: [
+           name: "Sugath Madurawe"
+         ],
+         description: 'Make sure to clean the sink when you are done!'
+       },
+       {
+         name: 'Vacuum Carpet'
+         assignedTo: [
+           name: "Jennifer Feng"
+         ],
+         description: 'Make sure to move the tables and chairs so that all areas are cleaned!'
+       }
+     ]
+   });
  };
