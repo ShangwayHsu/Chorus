4c9b264b5aac3e1828ee0bf4f328f15b356bdcb4
Shangway Hsu <shangway.hsu@gmail.com>
2017-02-10 15:11:03 -0800

    Implement view chore modal

---
+++ public/js/chorus.js
@@ -0,0 +1,95 @@
+//
+// MDL modal js
+//
+function showLoading() {
+    // remove existing loaders
+    $('.loading-container').remove();
+    $('<div id="orrsLoader" class="loading-container"><div><div class="mdl-spinner mdl-js-spinner is-active"></div></div></div>').appendTo("body");
+
+    componentHandler.upgradeElements($('.mdl-spinner').get());
+    setTimeout(function () {
+        $('#orrsLoader').css({opacity: 1});
+    }, 1);
+}
+
+function hideLoading() {
+    $('#orrsLoader').css({opacity: 0});
+    setTimeout(function () {
+        $('#orrsLoader').remove();
+    }, 400);
+}
+
+function showChore(options) {
+    options = $.extend({
+        id: 'orrsDiag',
+        title: null,
+        text: null,
+        people: null,
+        cancelable: true,
+        contentStyle: null,
+        onLoaded: false,
+        hideOther: true
+    }, options);
+
+    if (options.hideOther) {
+        // remove existing dialogs
+        $('.dialog-container').remove();
+        $(document).unbind("keyup.dialog");
+    }
+
+    $('<div id="' + options.id + '" class="dialog-container"><div class="mdl-card mdl-shadow--16dp" id="' + options.id + '_content"></div></div>').appendTo("body");
+    var dialog = $('#' + options.id);
+    var content = dialog.find('.mdl-card');
+    if (options.contentStyle != null) content.css(options.contentStyle);
+
+    // Putting content to modal
+    if (options.title != null) {
+        $('<h3>' + options.title + '</h3>').appendTo(content);
+    }
+    if (options.people != null) {
+        $('<p>' + "Assigned To:" + '</p>').appendTo(content);
+        var assignedPeople = '<div class="assigned-people">';
+        for (var i = 0; i < options.people.length; i++) {
+          assignedPeople += '<div class="assigned-person mdl-chip"><span class="mdl-chip__text">' + options.people[i] + '</span></div>';
+        }
+        assignedPeople += '</div>';
+        $(assignedPeople).appendTo(content);
+    }
+    if (options.text != null) {
+        $('<p>' + "Description:" + '</p>').appendTo(content);
+        $('<p>' + options.text + '</p>').appendTo(content);
+    }
+    var buttonBar = $('<div class="mdl-card__actions dialog-button-bar"></div>');
+
+    var bruhButton = $('<button class="bruh-button mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--colored mdl-color--red-600 mdl-color-text--white" id="bruhButton">' + "Bruhh" + '</button>');
+
+    bruhButton.appendTo(buttonBar);
+    buttonBar.appendTo(content);
+
+    componentHandler.upgradeDom();
+    if (options.cancelable) {
+        dialog.click(function () {
+            hideDialog(dialog);
+        });
+        $(document).bind("keyup.dialog", function (e) {
+            if (e.which == 27)
+                hideDialog(dialog);
+        });
+        content.click(function (e) {
+            e.stopPropagation();
+        });
+    }
+    setTimeout(function () {
+        dialog.css({opacity: 1});
+        if (options.onLoaded)
+            options.onLoaded();
+    }, 1);
+}
+
+function hideDialog(dialog) {
+    $(document).unbind("keyup.dialog");
+    dialog.css({opacity: 0});
+    setTimeout(function () {
+        dialog.remove();
+    }, 400);
+}
--- routes/choreDetails.js
+++ routes/choreDetails.js
@@ -1,7 +1,6 @@
 exports.view = function(req, res){
   var name = req.params.name; 
-
-  res.render('choreDetails', {
-    'choreName': name
-  });
+//  res.render('choreDetails', {
+//    'choreName': name
+//  });
 };
