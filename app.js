ec51aeff8aa9cd962e733113ada17a5af1f4626e
Shangway Hsu <shangway.hsu@gmail.com>
2017-02-19 17:44:26 -0800

    Trying to fix heroku problem

--- app.js
+++ app.js
@@ -409,6 +409,9 @@ app.delete('/api/groups/group=:groupId&user=:userId', function(req, res) {
 
 
 //----------- Serve -----------
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+//http.createServer(app).listen(app.get('port'), function(){
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
